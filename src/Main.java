5e5ff6d53547c990831a32deaa80491ace661ff6
Bogdan Sinyavskiy <sinavskijb@gmail.com>
2022-10-15 14:01:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        char a = 65;
+        char b = 66;
+        char c = 67;
+        char v = 'B';
+        System.out.println(a+""+b+""+c);
+//        System.out.print(b);
+//        System.out.print(c);
+        System.out.println(b+v);
+        float hh = 7f % 3f;
+        float hh1 = 7f / 3f;
+        System.out.println(hh);
+        System.out.println(hh1);
+        int hh2 = 7 % 3;
+        int hh3 = 7 / 3;
+        System.out.println(hh2);
+        System.out.println(hh3);
+       byte happyValue = 5;
+        System.out.println((2>5)  &&  ('A' <'B'));
+
+
+                byte happyValue1 = 8;
+                short cold = -50;
+                int globalAnswer = 42;
+                int randomAnswer;
+                randomAnswer = 700;
+                long longerThanLife =  214748364715L;
+
+
+
+                //Example output
+                System.out.println("happyValue = " + happyValue);
+                System.out.println("cold = " + cold);
+                System.out.println("globalAnswer = " + globalAnswer);
+                System.out.println("randomAnswer = " + randomAnswer);
+                System.out.println("longerThanLife = " + longerThanLife);
+        char englishA = 65;
+        char anyRuLetter = 160;
+        char codeName70 = 70;
+            }
+        }
